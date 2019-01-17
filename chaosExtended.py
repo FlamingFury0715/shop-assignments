f720ca71bca88bda6489fe584700df6e22279b5e
FlamingFury0715 <45069605+FlamingFury0715@users.noreply.github.com>
2019-01-17 12:31:18 -0500

    Create chaosExtended.py

---
+++ chaosExtended.py
@@ -0,0 +1,13 @@
+#Chaos.py
+#1/17/19
+#Phoenix Iserman
+#a simple program with chaotic behavior
+
+def main():
+    print("this program illustrates a chaotic function")
+    x = eval(input("enter a number between 0 and 1: "))
+    for i in range(20) :
+        x = 2.0 * x * (1 - x)
+        print(x)
+
+main()
